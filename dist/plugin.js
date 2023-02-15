7f826f99ae72df60bd2e41ad0c24db0a06e19686
IlyaBOT <isarbeu@gmail.com>
2023-02-15 13:23:53 +0300

    Update

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,5 +1,5 @@
-exports.version = 1.1
-exports.description = "Mini audio player by IlyaBOT"
+exports.version = 1.2
+exports.description = "Mini audio player [Reworked simple-player by IlyaBOT]"
 exports.apiRequired = 5
 exports.repo = "IlyaBOT/mini-player"
 exports.frontend_css = 'style.css'
